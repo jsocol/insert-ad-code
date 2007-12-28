fb3b24bc7180c47ea85ce902184c6be2729bf07f
james <james@91e5f8d4-5e63-498a-8cae-92d69a92f665>
2007-12-28 03:53:31 +0000

    Added Localization
    git-svn-id: svn://svn.jamessocol.com/insert_ad_code/trunk@9 91e5f8d4-5e63-498a-8cae-92d69a92f665

--- insert_ad_code.php
+++ insert_ad_code.php
@@ -3,7 +3,7 @@
  * Plugin Name: Insert Ad Code
  * Plugin URI: http://jamessocol.com/blog/2007/12/wordpress-plugin-insert-ad-code.php
  * Description: Automatically inserts ad code (ie: from Openads, AdSense, etc) into posts.
- * Version: 1.0.5
+ * Version: 1.1.0
  * Author: James Socol
  * Author URI: http://jamessocol.com/
  */
@@ -25,18 +25,20 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-$js_insert_ad_code_version = '1.0.5';
+$js_insert_ad_code_version = '1.1.0';
+$js_insert_ad_code_textdomain = 'js_insert_ad_code';
 
 // Add the page to the options menu
 function js_insert_ad_code_admin_menu() {
-	add_options_page('Insert Ad Code Manager', 'Insert Ad Code', 'manage_options', __FILE__, 'js_insert_ad_code_admin_page');
+	global $js_insert_ad_code_textdomain;
+	add_options_page(__('Insert Ad Code Manager', $js_insert_ad_code_textdomain), __('Insert Ad Code', $js_insert_ad_code_textdomain), 'manage_options', __FILE__, 'js_insert_ad_code_admin_page');
 }
 
 /**
  * The Admin page
  */
 function js_insert_ad_code_admin_page() {
-	global $js_insert_ad_code_version;
+	global $js_insert_ad_code_version, $js_insert_ad_code_textdomain;
 	$optionvars = array(
 		'js_insert_ad_code_enable',
 		'js_insert_ad_code_type',
@@ -44,37 +46,31 @@ function js_insert_ad_code_admin_page() {
 		'js_insert_ad_code_html');
 ?>
 <div class="wrap">
-	<h2>Insert Ad Code Manager</h2>
-	<p><strong>Insert Ad Code</strong> helps turn your blog into a money-maker by 
-		automatically inserting ads&mdash;or any other HTML&mdash;into your posts.</p>
-	<p><strong>Insert Ad Code</strong> is meant to be used with a program like
-		<a href="http://google.com/adsense/" title="Google AdSense" target="_blank">Google AdSense</a> or an external script
-		like <a href="http://www.openads.org/" title="Openads Ad Server" target="_blank">Openads</a>.
-		<strong>Insert Ad Code</strong> does not rotate ads, nor keep track of impressions,
-		clicks, or conversions. It just inserts code.</p>
+	<h2><?php _e('Insert Ad Code Manager', $js_insert_ad_code_textdomain); ?></h2>
+	<p><strong><?php _e('Insert Ad Code', $js_insert_ad_code_textdomain); ?></strong> <?php _e('helps turn your blog into a money-maker by automatically inserting ads&mdash;or any other HTML&mdash;into your posts.', $js_insert_ad_code_textdomain); ?></p>
+	<p><strong><?php _e('Insert Ad Code', $js_insert_ad_code_textdomain); ?></strong> <?php _e('is meant to be used with a program like <a href="http://google.com/adsense/" title="Google AdSense" target="_blank">Google AdSense</a> or an external script like <a href="http://www.openads.org/" title="Openads Ad Server" target="_blank">Openads</a>. <strong>Insert Ad Code</strong> does not rotate ads, nor keep track of impressions, clicks, or conversions. It just inserts code.', $js_insert_ad_code_textdomain); ?></p>
 	<form action="options.php" method="post">
 		<?php if (function_exists('wp_nonce_field')) { wp_nonce_field('update-options'); } ?>
 		<input type="hidden" name="action" value="update" />
 		<input type="hidden" name="page_options" value="<?php echo join(',', $optionvars); ?>"/>
 		
 		<!-- enable or disable insert ad code -->
-		<p>Enable <strong>Insert Ad Code</strong>?<br />
-			<input type="radio" name="js_insert_ad_code_enable" id="js_insert_ad_code_enable_yes" value="1"<?php if (get_option('js_insert_ad_code_enable')==1):?> checked="checked"<?php endif; ?> /> <label for="js_insert_ad_code_enable_yes"><strong>Yes</strong></label> &nbsp;
-			<input type="radio" name="js_insert_ad_code_enable" id="js_insert_ad_code_enable_no" value="0"<?php if ( get_option('js_insert_ad_code_enable')!=1):?> checked="checked"<?php endif;?> /> <label for="js_insert_ad_code_enable_no"><strong>No</strong></label></p>
+		<p><?php _e('Enable'); ?> <strong><?php _e('Insert Ad Code', $js_insert_ad_code_textdomain); ?></strong>?<br />
+			<input type="radio" name="js_insert_ad_code_enable" id="js_insert_ad_code_enable_yes" value="1"<?php if (get_option('js_insert_ad_code_enable')==1):?> checked="checked"<?php endif; ?> /> <label for="js_insert_ad_code_enable_yes"><strong><?php _e('Yes', $js_insert_ad_code_textdomain); ?></strong></label> &nbsp;
+			<input type="radio" name="js_insert_ad_code_enable" id="js_insert_ad_code_enable_no" value="0"<?php if ( get_option('js_insert_ad_code_enable')!=1):?> checked="checked"<?php endif;?> /> <label for="js_insert_ad_code_enable_no"><strong><?php _e('No', $js_insert_ad_code_textdomain); ?></strong></label></p>
 		
 		<!-- use 'more' tags or custom tags -->
-		<p>Should I insert ads after the WordPress <strong>&lt;!--more--&gt;</strong> tag, 
-			or should I use a custom tag (see below)?<br />
+		<p><?php _e('Should I insert ads after the WordPress', $js_insert_ad_code_textdomain); ?> <strong>&lt;!--more--&gt;</strong> <?php _e('tag, or should I use a custom tag (see below)?', $js_insert_ad_code_textdomain); ?><br />
 			<input type="radio" name="js_insert_ad_code_type" id="js_insert_ad_code_type_more" value="more"<?php if (get_option('js_insert_ad_code_type')=='more'):?> checked="checked"<?php endif; ?> /> <label for="js_insert_ad_code_type_more"><strong>&lt;!--more--&gt;</strong></label> &nbsp;
 			<input type="radio" name="js_insert_ad_code_type" id="js_insert_ad_code_type_custom" value="custom"<?php if ( get_option('js_insert_ad_code_type')!='more'):?> checked="checked"<?php endif;?> /> <label for="js_insert_ad_code_type_custom"><strong><?php echo htmlspecialchars(get_option('js_insert_ad_code_custom_tag')); ?></strong></label></p>
 		
 		<!-- define a custom tag -->
-		<p>If I should look for a custom tag, what should the tag be?<br />
-			<input type="text" name="js_insert_ad_code_custom_tag" id="js_insert_ad_code_custom_tag" value="<?php echo get_option('js_insert_ad_code_custom_tag'); ?>" style="width: 50%" /> <label for="js_insert_ad_code_custom_tag"><strong>Custom Tag</strong></label> 
-			<small>(<a href="javascript:;" onclick="document.getElementById('js_insert_ad_code_custom_tag').value='&lt;!--insert ads--&gt;';">default</a>)</small></p>
+		<p><?php _e('If I should look for a custom tag, what should the tag be?', $js_insert_ad_code_textdomain); ?><br />
+			<input type="text" name="js_insert_ad_code_custom_tag" id="js_insert_ad_code_custom_tag" value="<?php echo get_option('js_insert_ad_code_custom_tag'); ?>" style="width: 50%" /> <label for="js_insert_ad_code_custom_tag"><strong><?php _e('Custom Tag', $js_insert_ad_code_textdomain); ?></strong></label> 
+			<small>(<a href="javascript:;" onclick="document.getElementById('js_insert_ad_code_custom_tag').value='&lt;!--insert ads--&gt;';"><?php _e('default'); ?></a>)</small></p>
 		
 		<!-- finally, what should the code be? -->
-		<p><label for="js_insert_ad_code_html">What code should I insert?</label><br />
+		<p><label for="js_insert_ad_code_html"><?php _e('What code should I insert?', $js_insert_ad_code_textdomain); ?></label><br />
 			<textarea name="js_insert_ad_code_html" id="js_insert_ad_code_html" style="width: 50%; height: 200px" wrap="virtual"><?php echo get_option('js_insert_ad_code_html'); ?></textarea></p>
 		
 		<!-- submit the form -->
@@ -82,7 +78,7 @@ function js_insert_ad_code_admin_page() {
 			<input type="submit" name="Submit" value="<?php _e('Update Options &raquo;') ?>" />
 		</p>
     </form>
-	<p><strong>Insert Ad Code <?php echo $js_insert_ad_code_version; ?></strong> by <a href="http://jamessocol.com">James Socol</a>.</p>
+	<p><strong><?php _e('Insert Ad Code', $js_insert_ad_code_textdomain); ?> <?php echo $js_insert_ad_code_version; ?></strong> <?php _e('by', $js_insert_ad_code_textdomain); ?> <a href="http://jamessocol.com">James Socol</a>.</p>
 </div>
 <?php
 }
@@ -146,6 +142,7 @@ function js_insert_ad_code_deactivate () {
 }
 
 // Register everything
+load_plugin_textdomain($js_insert_ad_code_textdomain, 'wp-content/plugins/insert_ad_code');
 register_activation_hook(__FILE__, 'js_insert_ad_code_activate');
 register_deactivation_hook(__FILE__, 'js_insert_ad_code_deactivate');
 add_action('admin_menu', 'js_insert_ad_code_admin_menu');
